9e5039a154c8220a783fce059bd3b6efcbda221b
Umidjon-Frontend <96024729+Umidjon-Frontend@users.noreply.github.com>
2022-08-20 19:20:36 +0500

    Update Number.js

--- Numbers.js
+++ Numbers.js
@@ -3,3 +3,154 @@
 // let  a = 123e5; // 12300000
 // let b = 123e-5; // 0.00123
 
+// let x = 0.2 + 0.1;
+// console.log(x);
+
+// let x = (0.2 * 10 + 0.1 * 10) / 10;
+// console.log(x);
+
+// let x = 10;
+// let y = 20;
+// let z = x + y;
+// console.log(z);
+
+// let x = "10";
+// let y = "20";
+// let z = x + y;
+// console.log(z);
+
+// let x = 10;
+// let y = "20";
+// let z = x + y;
+// console.log(z);
+
+// let x = "10";
+// let y = 20;
+// let z = x + y;
+// console.log(z);
+
+// let x = 10;
+// let y = 20;
+// let z = "The result is: " + x + y;
+// console.log(z);
+
+// let x = 10;
+// let y = 20;
+// let z = "30";
+// let result = x + y + z;
+// console.log(result);
+
+// let x = 100; // x is a number
+// let y = "100" // y is a string
+
+// let x = "100";
+// let y = "10";
+// let z = x / y;
+// console.log(z);
+
+// let x = "100";
+// let y = "10";
+// let z = x * y;
+// console.log(z);
+
+// let x = "100";
+// let y = "10";
+// let z = x - y;
+// console.log(z);
+
+// let x = "100";
+// let y = "10";
+// let z = x + y;
+// console.log(z);
+
+// NaN - Not a Number
+// let x = 100 / "Apple";
+
+// let x = 100 / "10";
+
+// let x = 100 / "Apple";
+// y = isNaN(x) // true
+
+// let x = NaN;
+// let y = 5;
+// let z = x + y;
+
+// let x = NaN;
+// let y = "5";
+// let z = x + y;
+
+
+// console.log(typeof NaN);
+
+// let x =  2 / 0;
+// console.log(x);
+// console.log(typeof Infinity);
+
+// let y = -2 / 0;
+// console.log(y);
+// console.log(typeof -Infinity);
+
+// let myNumber = 15;
+// console.log(myNumber.toString(32));
+// console.log(myNumber.toString(16));
+// console.log(myNumber.toString(12));
+// console.log(myNumber.toString(10));
+// console.log(myNumber.toString(8));
+// console.log(myNumber.toString(2));
+
+// Number Methods
+// let x = 123;
+// let y = x.toString();
+// console.log(typeof y);
+
+// let x = 9.656;
+// console.log(x.toExponential(2));
+// console.log(x.toExponential(4));
+// console.log(x.toExponential(6));
+
+// let x = 9.656;
+// console.log(x.toFixed(0));
+// console.log(x.toFixed(2));
+// console.log(x.toFixed(4));
+// console.log(x.toFixed(6));
+
+// let x = 9.656;
+// console.log(x.toPrecision());
+// console.log(x.toPrecision(2));
+// console.log(x.toPrecision(4));
+// console.log(x.toPrecision(6));
+
+// let x = 123;
+// y = x.valueOf();
+// console.log(typeof y);
+
+// Converting Variables to Numbers
+// Number()
+// parseInt()
+// parseFloat()
+
+// console.log(Number(true))
+// console.log(Number(false))
+// console.log(Number("10"))
+// console.log(Number("  10"))
+// console.log(Number("10  "))
+// console.log(Number(" 10  "))
+// console.log(Number("10.33"))
+// console.log(Number("10,33"))
+// console.log(Number("10 33"))
+// console.log(Number("John"))
+
+// console.log(parseInt("-10"))
+// console.log(parseInt("-10.33"))
+// console.log(parseInt("10"))
+// console.log(parseInt("10.33"))
+// console.log(parseInt("10 20 30"))
+// console.log(parseInt("10 years"))
+// console.log(parseInt("years 10"))
+
+// console.log(parseFloat("10"))
+// console.log(parseFloat("10.33"))
+// console.log(parseFloat("10 20 30"))
+// console.log(parseFloat("10 years"))
+// console.log(parseFloat("years 10"))
+
